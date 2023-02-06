e017c2675fd310d30e9e4160692658b71962f72a
Levi <124380729@qq.com>
2023-02-06 22:03:14 +0800

    feat: 引入vercel埋点功能

--- packages/levi-docs/docs/.vitepress/theme/index.ts
+++ packages/levi-docs/docs/.vitepress/theme/index.ts
@@ -3,7 +3,11 @@ import { install } from "naive-ui"
 import { onMounted } from 'vue'
 import mediumZoom from 'medium-zoom'
 import { Playground } from '../../components'
+import { inject } from '@vercel/analytics';
 import './custom.css'
+
+// 引入vercel埋点
+inject()
 
 export default {
   ...DefaultTheme,
