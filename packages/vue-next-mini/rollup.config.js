5cc9d754e134975b6d339f0c142505845e7d60fd
Levi <124380729@qq.com>
2023-02-08 23:37:48 +0800

    docs: js和ts的补充

--- packages/vue-next-mini/rollup.config.js
+++ packages/vue-next-mini/rollup.config.js
@@ -1,5 +1,34 @@
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import typescript from '@rollup/plugin-typescript'
+
 export default [
   {
     // 入口文件
+    input: 'packages/vue/src/index.ts',
+    // 打包出口
+    output: [
+      // 导出iife模式的包
+      {
+        // 开启sourcemap
+        sourcemap: true,
+        // 导出文件地址
+        file: './packages/vue/dist/vue.js',
+        // 生成包的格式
+        format: 'iife',
+        // 变量名
+        name: 'Vue'
+      }
+    ],
+    // 插件
+    plugins: [
+      typescript({
+        sourceMap: true
+      }),
+      // 模块导入路径补全
+      resolve(),
+      // cjs 转为 esm
+      commonjs()
+    ]
   }
 ]
