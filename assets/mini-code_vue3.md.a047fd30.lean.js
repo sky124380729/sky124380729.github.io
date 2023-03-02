3cb351ef1b37554f7fbfa4d46882296810889253
sky124380729 <sky124380729@users.noreply.github.com>
2023-03-02 15:43:53 +0000

    deploy: 03513858bb304b7a0e6678a9b88933889316bf6e

---
+++ assets/mini-code_vue3.md.a047fd30.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,o as n,c as a,a as l}from"./app.8e5e362f.js";const i=JSON.parse('{"title":"Mini Vue3","description":"","frontmatter":{},"headers":[],"relativePath":"mini-code/vue3.md","lastUpdated":1677771756000}'),p={name:"mini-code/vue3.md"},o=l("",3),e=[o];function t(c,r,F,y,D,A){return n(),a("div",null,e)}const d=s(p,[["render",t]]);export{i as __pageData,d as default};
