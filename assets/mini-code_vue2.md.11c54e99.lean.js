7027ab51ca71b4ddc77d454eb4ac18f5d9ed5d7d
sky124380729 <sky124380729@users.noreply.github.com>
2023-03-13 15:32:08 +0000

    deploy: 0c5c3b5edeb6dd642eea2953676a575f921b37a5

--- assets/mini-code_vue2.md.11c54e99.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,o as n,c as a,a as l}from"./app.b5b89a77.js";const i=JSON.parse('{"title":"Mini Vue2","description":"","frontmatter":{},"headers":[],"relativePath":"mini-code/vue2.md","lastUpdated":1678703091000}'),p={name:"mini-code/vue2.md"},o=l("",3),t=[o];function e(c,F,r,y,D,A){return n(),a("div",null,t)}const d=s(p,[["render",e]]);export{i as __pageData,d as default};
